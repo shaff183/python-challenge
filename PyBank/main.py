c1d199fc54b667fc1c70f288d956e2e65fe091ac
Eric Shaffer <EricShaffer@Chriss-MacBook-Pro-3.local>
2021-01-16 13:52:08 -0600

    Initial Versions of PyPoll and PyBank

---
+++ PyBank/main.py
@@ -0,0 +1,77 @@
+# Importing OS and CSV
+import os 
+import csv 
+
+# Set the file path for the data 
+budget_file = os.path.join("Resources", "budget_data.csv")
+
+financialData = []
+
+def analyze_data(financialData):
+    print("Financial Analysis")
+    print("-----------------------------------")
+
+    # get the total number of months in the data set
+    total_months = len(financialData)
+
+    print(f"Total Months: {total_months}")
+
+    # Find the total profits in the dataset 
+    total_profit_losses = 0
+
+    for months in financialData:                   
+        total_profit_losses += int(months[1])
+
+    print(f"Total: ${total_profit_losses}")
+
+
+    # Calculate the change in "profit/Losses" over the entire period, then find the average of those changes
+
+    # Create a temporary list of just the profits and losses and the change within those profits
+    temporary_list = []
+    change_in_profits_list = []
+
+    for months in financialData:        
+        temporary_list.append(int(months[1])) 
+
+    i = 0 # Using i to be able to signify both the position we are at in the list as well as the next position
+
+    for numbers in range(len(temporary_list) - 1):
+        change_in_profits_list.append(int(temporary_list[i+1]) - int(temporary_list[i]))
+        i += 1
+    
+    average_change = sum(change_in_profits_list) / len(change_in_profits_list)
+
+    print(f"Average Change: ${average_change:.2f}")
+
+    # Finding the greatest increase in profits
+    greatest_increase_in_profits = max(change_in_profits_list)
+
+    index_increase_change = change_in_profits_list.index(greatest_increase_in_profits) + 1
+
+    print(f"Greatest Increase in Profits: {financialData[index_increase_change][0]} - ${greatest_increase_in_profits}")
+
+    # Finding the greatest decrease in profits
+    greatest_decrease_in_profits = min(change_in_profits_list)
+
+    index_decrease_change = change_in_profits_list.index(greatest_decrease_in_profits) + 1
+
+    print(f"Greatest Decrease in Profits: {financialData[index_decrease_change][0]} - ${greatest_decrease_in_profits}")
+
+
+# Opening the file path to start working with the data
+with open(budget_file, "r") as csvfile:
+
+    # Allow us to read into the data file
+    budget_reader = csv.reader(csvfile, delimiter=",")
+
+    # NEED TO SKIP THE HEADER FOR THE FUNCTION TO WORK PROPERLY
+    header = next(budget_reader, None)
+
+    # Reading the budget_reader into a new list so we can use the functions we need
+    for row in budget_reader:
+        financialData.append(row)
+
+    # Calling the function
+    analyze_data(financialData)
+    
---
+++ PyPoll/main.py
@@ -0,0 +1,73 @@
+# Import OS and CSV
+import os 
+import csv
+
+## Use a dictionary for number of votes per candidate and also percentage of votes per candidate ##
+
+# open the file path 
+election_file = os.path.join("Resources", "election_data.csv")
+
+election_list = []
+
+def poll_analysis(election_list):
+    # Formatting
+    print("Election Results")
+    print("---------------------------")
+
+    # Find the total number of votes cast
+    total_votes = len(election_list)
+    print(f'Total Votes: {total_votes}')
+
+    print("---------------------------")
+
+    # Create a dictionary of candidates who received votes and count their votes
+    candidates_votes = {}
+
+    for candidates in election_list:
+        if candidates[2] in candidates_votes:
+            candidates_votes[candidates[2]] += 1
+        else:
+            candidates_votes[(candidates[2])] = 1
+
+    # Create another dictionary with candidates and their percentage of votes
+    candidates_percent = dict([(key, (value / total_votes) * 100) for key, value in candidates_votes.items()])
+
+    # Find the winner of the election based on popoular vote
+    candidate = list(candidates_percent.keys())
+    votes = list(candidates_percent.values())
+    winner = candidate[votes.index(max(votes))]
+
+    # A final list with both values in it for each candidate
+    final_list_candidates = list(candidates_percent.keys())
+    final_list_percents = list(candidates_percent.values())
+    final_list_votes = list(candidates_votes.values())
+
+    # Print each of the candidates, their percentage of votes, and the number of votes
+    i = 0
+    for row in final_list_candidates:
+        print(f"{final_list_candidates[i]}: {final_list_percents[i]:.2f}% ({final_list_votes[i]})")
+        i += 1
+
+    print("---------------------------")
+
+    print(f"Winner: {winner}")
+
+    print("---------------------------")
+
+
+
+# read the file to begin working with the data
+with open(election_file, "r") as csvfile:
+
+    election_reader = csv.reader(csvfile, delimiter=",")
+
+    # skipping the header but putting it in a variable
+    header = next(election_reader, None)
+
+    # Creating a new list that we will call the function on 
+    for row in election_reader:
+        election_list.append(row)
+    
+    # Calling our Function
+    poll_analysis(election_list)
+            
